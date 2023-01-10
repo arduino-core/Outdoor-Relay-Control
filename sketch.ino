9fbf8e5f4cb3e7768255097edfeb2438b7506c32
github host <110337548+github-host2@users.noreply.github.com>
2023-01-10 19:03:27 +0100

    Update sketch.ino

--- sketch.ino
+++ sketch.ino
@@ -8,3 +8,9 @@ void loop(){
 void pin(int pin, type){
   digitalWrite(pin, type);
 }
+
+void temperature_sensor(){
+  value = analogRead(A0);
+  temperature = map(value, 0, 410, -50, 150);
+  Serial.print(temperature);
+}
